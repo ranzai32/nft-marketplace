8d7ffcfde2082355643ba2ba75940fc85e17e8e7
mossimka <maksimsarsekeev@gmail.com>
2025-04-14 16:10:12 +0500

    continue, havbar compelted

--- nft-template/components/NavBar/NavBar.jsx
+++ nft-template/components/NavBar/NavBar.jsx
@@ -20,6 +20,55 @@ const NavBar = () => {
   const [profile, setProfile] = useState(false);
   const [openSideMenu, setOpenSideMenu] = useState(false);
 
+  const openMenu = (e) => {
+    const btnText = e.target.innerText;
+    if(btnText == 'Discover') {
+      setDiscover(true);
+      setHelp(false);
+      setNotification(false);
+      setProfile(false);
+    } else if(btnText == 'Help Center') {
+      setDiscover(false);
+      setHelp(true);
+      setNotification(false);
+      setProfile(false);
+    } else {
+      setDiscover(false);
+      setHelp(false);
+      setNotification(false);
+      setProfile(false);
+    }
+  }
+
+  const openNotification = () => {
+    if(!notification) {
+      setNotification(true);
+      setDiscover(false);
+      setHelp(false);
+      setProfile(false);
+    } else {
+      setNotification(false);
+    }
+  }
+
+  const openProfile = () => {
+    if(!profile) {
+      setProfile(true);
+      setDiscover(false);
+      setHelp(false);
+      setNotification(false);
+    } else {
+      setProfile(false);
+    }
+  }
+
+  const OpenSideBar = () => {
+    if(!openSideMenu) {
+      setOpenSideMenu(true);
+    } else {
+      setOpenSideMenu(false);
+    }
+  }
   return (
     <div className={Style.navbar}>
       <div className={Style.navbar_container}>
@@ -37,7 +86,67 @@ const NavBar = () => {
         {/* // End of left section*/}
         
         <div className={Style.navbar_container_right}>
-          
+          <div className={Style.navbar_container_right_discover}>
+            {/* Discover Menu*/}
+            <p onClick={(e) => openMenu(e)}>Discover</p>
+            {discover && (
+              <div className={Style.navbar_container_right_discover_box}>
+                <Discover />
+              </div>
+            )}
+            
+            {/*Help Center Menu*/}
+            <div className={Style.navbar_container_right_help}>
+              <p onClick={(e) => openMenu(e)}>Help Center</p>
+              {help && (
+                <div className={Style.navbar_container_right_help_box}>
+                  <HelpCenter />
+                </div>
+              )}
+            </div>
+
+            {/*Notification*/}
+            <div className={Style.navbar_container_right_notify}>
+              <MdNotifications className={Style.notify} onClick={() => openNotification()}/>
+              {notification && <Notification />}
+            </div>
+
+            {/*Create button section*/}
+            <div className={Style.navbar_container_right_button}>
+              <Button btnText='Create'/>
+            </div>
+
+            {/*Profile section*/}
+            <div className={Style.navbar_container_right_profile_box}>
+              <div className={Style.navbar_container_right_profile}>
+                <Image 
+                  src={images.user1} 
+                  alt="Profile" 
+                  width={40} 
+                  height={40} 
+                  onClick={() => openProfile()}
+                  className={Style.navbar_container_right_profile}
+                />
+
+                {profile && <Profile />}
+              </div>
+            </div>
+
+            {/*Hamburger menu(Mobile)*/}
+            <div className={Style.navbar_container_right_menuBtn}>
+              <CgMenuRight  
+                className={Style.menuIcon}
+                onClick={() => OpenSideBar()} 
+              />
+            </div>
+
+            {/*Sidebar*/}
+            {openSideMenu && (
+              <div className={Style.sidebar}>
+                <Sidebar setOpenSideMenu={setOpenSideMenu} />
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
