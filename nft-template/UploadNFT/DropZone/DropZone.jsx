b911430c06fed16b457c64b14b91a6dfa19cb360
mossimka <maksimsarsekeev@gmail.com>
2025-05-02 10:40:53 +0500

    Upload nft page(unclompleted)

--- nft-template/UploadNFT/DropZone/DropZone.jsx
+++ nft-template/UploadNFT/DropZone/DropZone.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {useState, useMemo, useCallback, useContext} from 'react';
+import { useDropzone } from 'react-dropzone';
 
-import Style from './DropZone.module.css'
+import Style from './DropZone.module.css';
 
-const DropZone = () => {
+const DropZone = ({ title, heading, subHeading, itemName,
+        website, description, royalties, fileSize,
+        category, properties,image }) => {
   return (
     <div>DropZone</div>
   )
